1aa3891dd4379e5593ee6dc47941ec24e25d2c7c
thesrtagomes <pamela.gomes.work@gmail.com>
2024-10-04 23:14:18 -0300

    finish week 3 project

--- scripts/place.js
+++ scripts/place.js
@@ -7,4 +7,27 @@ const lastUpdatedElement = document.getElementById("lastupdated")
 lastUpdatedElement.innerHTML = document.lastModified
 
 
-// create a function called calculateWindChill
+const temperatureLabel = document.querySelector("#temperature-label")
+const conditionsLabel = document.querySelector("#conditions-label")
+const windLabel =  document.querySelector("#wind-label")
+const windChillLabel = document.querySelector("#wind-chill-label")
+
+
+const calculateWindChill = (temperature, windSpeed) => {
+    return 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+}
+
+const temperature = 5
+const windSpeed = 6
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChillLabel.innerHTML = calculateWindChill(temperature, windSpeed).toFixed(1) + " °C";
+}
+else {
+    windChillLabel.innerHTML = "N/A"
+}
+
+temperatureLabel.innerHTML = temperature + " °C";
+conditionsLabel.innerHTML = "Sunny";
+windLabel.innerHTML = windSpeed + " km/h";
+
