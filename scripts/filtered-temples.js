9a3f2e7305eeb6e671b38749d406f10c0897eb64
thesrtagomes <pamela.gomes.work@gmail.com>
2024-10-11 18:22:05 -0300

    fixing lighthouse issues

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -112,6 +112,8 @@ function displayTemples(temples) {
         img.src = temple.imageUrl;
         img.alt = temple.templeName;
         img.loading = 'lazy';
+        img.width = 400;
+        img.height = 250;
 
         const caption = document.createElement('figcaption');
         caption.innerHTML = `
